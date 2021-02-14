178ba554137ba025ca3406284188c1ea4a9a3a30
Oscar Miticanoy <oscarmiticanoy@gmail.com>
2021-02-13 19:28:57 -0500

    Add project files.

---
+++ ClienteRazor/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ClienteRazor.Models;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ClienteRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        List<Result> person = new List<Result>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string Url = "https://localhost:44326/api/agreement";
+            WebClient wc = new WebClient();
+            var perso = wc.DownloadString(Url);
+            var rs = JsonConvert.DeserializeObject<Persona>(perso);
+            foreach(var pe in rs.results)
+            {
+                person.Add(pe);
+            }
+            return View(person);
+        }
+
+        public IActionResult gridGetData()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ClienteRazor/Models/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ClienteRazor.Models
+{
+    public class Result
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("amount")]
+        public int Amount { get; set; }
+    }
+
+    public class Persona
+    {
+        public List<Result> results { get; set; }
+
+    }
+}
